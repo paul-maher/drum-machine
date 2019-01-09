4313a421f602ef8e998e3d54ff609664cce601bc
paul-maher <paul.maher@open.ac.uk>
2019-01-09 16:17:50 +0000

    Add code to list all the relevant methods from the class automatically and call them at random

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from drummachine import DrumMachine
 import time 
+import random
 
-# Create a new drum machine object
-dm = DrumMachine()
+def playRandom(dm:DrumMachine, n:int):
+
+	# Get a list of methods in the Rhythms class that start with the letters 'play' 
+	method_list = [func for func in dir(dm) if callable(getattr(dm, func)) and func.startswith("play")]
+	print(method_list)
+
+	# Now loop round n times, generate a random number and call that method from the list of methods
+	for _ in range(n):
+		getattr(dm,method_list[random.randint(0,len(method_list)-1)])()
 
 #dm.thump(4,10)
 #dm.testAll()
 
 #dm.playRhythm7()
 
-dm.playRandomRhythm(4)
+#dm.playRandomRhythm(4)
+
+# Create a new drum machine object
+dm = DrumMachine()
+playRandom(dm,8)
